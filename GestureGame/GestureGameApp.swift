edfcfcff0667e548b83fb6ddafd0020ccc90e10a
dejes <jack940197@gmail.com>
2021-04-07 14:06:20 +0800

    Initial Commit

---
+++ GestureGame/GestureGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureGameApp.swift
+//  GestureGame
+//
+//  Created by User14 on 2021/4/7.
+//
+
+import SwiftUI
+
+@main
+struct GestureGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
